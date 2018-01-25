b3c8d1e0f8f43b9ed2070b137da19ff16c4f3a12
Miguel Montoya <miguel.mzbi@gmail.com>
2018-01-24 21:26:53 -0600

    Remote-Mongo

--- web-services/index.js
+++ web-services/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
 const o2x = require('object-to-xml');
+const mongoURL = "mongodb://santaclosdemo:santaclospass@ds113648.mlab.com:13648/santoclos-demo"
+const mLab = require('mongolab-data-api')('oQbIzLHtooOzykPrjE-Zqw9N-dnzCSvt');
 
 var database = {
     users: {
@@ -17,7 +19,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.route("/xml")
 .get((req, res, next) => {
-    let data = database.users[(req.query["name"] || "default")];
+
+    let data;
+
+    var optionsDB = {
+        database: 'santoclos-demo',
+        collectionName: 'database',
+    };
+    mLab.listDocuments(optionsDB, function (err, collections) {
+        data = database.users[(req.query["name"] || "default")];
+    });
+
     res.set('Content-Type', 'text/xml');
     res.send(o2x({
         '?xml version="1.0" encoding="utf-8"?': null,
@@ -27,7 +39,15 @@ app.route("/xml")
 
 app.route("/json")
 .get((req, res, next) => {
-    let returnData = database.users[(req.query["name"] || "default")];
+    let returnData;
+
+    var optionsDB = {
+        database: 'santoclos-demo',
+        collectionName: 'database',
+    };
+    mLab.listDocuments(optionsDB, function (err, collections) {
+        returnData = database.users[(req.query["name"] || "default")];
+    });
     res.json(returnData);
 });
 
